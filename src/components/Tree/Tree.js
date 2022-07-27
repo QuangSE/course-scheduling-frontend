fe5743618e688266d4eb30b105df9d2982743ee0
Quang <do_quang-tung@web.de>
2022-07-27 03:49:45 +0200

    feat: initial design structure of editable cells (does not work yet)

--- src/components/Tree/Tree.js
+++ src/components/Tree/Tree.js
@@ -2,60 +2,60 @@ import React, { useState } from "react";
 import "./tree.css";
 
 const Tree = ({ data = [] }) => {
-    return (
-        <div className="">
-            <ul className="">
-                {data.map((tree) => (
-                    <TreeNode node={tree} />
-                ))}
-            </ul>
-        </div>
-    );
+  return (
+    <div className="">
+      <ul className="">
+        {data.map((tree) => (
+          <TreeNode node={tree} />
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 const TreeNode = ({ node }) => {
-    const [childVisible, setChildVisiblity] = useState(false);
+  const [childVisible, setChildVisiblity] = useState(false);
 
-    const hasChild = node.children ? true : false;
+  const hasChild = node.children ? true : false;
 
-    return (
-        <li className="no-bullets">
+  return (
+    <li className="no-bullets">
+    <div
+        className="first-item"
+        onClick={(e) => setChildVisiblity((v) => !v)}
+    >
+        {hasChild && (
             <div
-                className="first-item"
-                onClick={(e) => setChildVisiblity((v) => !v)}
-            >
-                {hasChild && (
-                    <div
-                        className={`tree-toggler ${
-                            childVisible ? "active" : ""
-                        }`}
-                    ></div>
-                )}
+                className={`tree-toggler ${
+                    childVisible ? "active" : ""
+                }`}
+            ></div>
+        )}
 
-                <div>{node.name}</div>
-            </div>
+        <div>{node.name}</div>
+    </div>
 
-            {hasChild && childVisible && !node.module && (
-                <div className="child">
-                    <ul className="child">
-                        <Tree data={node.children} />
-                    </ul>
-                </div>
-            )}
-            {hasChild && childVisible && node.module && (
-                <div className="child">
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>Modul</th>
-                                <th>Lehrveranstaltung</th>
-                            </tr>
-                        </thead>
-                    </table>
-                </div>
-            )}
-        </li>
-    );
+    {hasChild && childVisible && !node.module && (
+        <div className="child">
+            <ul className="child">
+                <Tree data={node.children} />
+            </ul>
+        </div>
+    )}
+    {hasChild && childVisible && node.module && (
+        <div className="child">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Modul</th>
+                        <th>Lehrveranstaltung</th>
+                    </tr>
+                </thead>
+            </table>
+        </div>
+    )}
+</li>
+  );
 };
 
 export default Tree;
